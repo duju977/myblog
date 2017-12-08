abe39ae0350d37660a760fdda5158ec1301195dc
薛荃荃 <1304499860@qq.com>
2017-12-08 12:01:25 +0800

    数据库配置成功了

--- app.js
+++ app.js
@@ -4,27 +4,30 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+//引入数据库配置文件
+var setting = require('./setting');
+//引入数据库操作实例
+var db = require('./model/db');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+//添加路由对象
+var routes = require('./routes/index');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
-
+//将app传到路由函数使用
+routes(app);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -43,4 +46,9 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+
+//监听
+app.listen(3000,function(){
+  console.log('node is ok')
+})
 module.exports = app;
